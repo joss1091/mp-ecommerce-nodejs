3bbd12b0012b36016aa8a3e087595828e01fc416
Jose Vergara <joss1091@gmail.com>
2020-11-26 10:13:49 -0500

    crear preferencia

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
+const mercadopago = require('mercadopago');
+mercadopago.configure({
+    access_token: 'APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181'
+});
+
 
 var app = express();
- 
+// var baseUrl = "https://joss1091-mp-commerce-nodejs.herokuapp.com/"
+ var baseUrl = "http://localhost:3000/"
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -16,7 +22,72 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    var img = req.query.img.replace("./","/")
+    var global = {}
+    let preference = {
+        items: [{
+            id: "1234",
+            title: req.query.title,
+            unit_price: parseFloat(req.query.price),
+            quantity: 1,
+            description: "Dispositivo móvil de Tienda e-commerce",
+            pickture_url: baseUrl + img
+        }],
+        external_reference: "joss1091@gmail.com",
+        payer: {
+            name: "Lalo Landa",
+            email: "test_user_81131286@testuser.com",
+            phone: {
+                area_code: "52",
+                number: parseInt("5549737300")
+            },
+            address: {
+                zip_code: "03940",
+                street_name: "Insurgentes Sur",
+                street_number: 1602
+            }
+        },
+        back_urls: {
+            success: baseUrl + "payment-sucess" ,
+            pending: baseUrl + "payment-pending",
+            failure: baseUrl + "payment-failure"
+        },
+        payment_methods: {
+            installments: 6,
+            excluded_payment_methods: [
+                {id: "amex"}
+            ],
+            excluded_payment_types: [
+                {id: "atm"}
+            ]
+        }
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(function (response) {
+            console.log(response)
+            // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+            global.id = response.body.id;
+        }).catch(function (error) {
+            console.log(error);
+        });
+    res.render('detail', global);
 });
+
+app.get("/process-payment",function(req,res){
+    res.render("payment",req.query)
+})
+
+app.get("payment-success", function(req, res){
+    res.render("suceess", req.query)
+})
+
+app.get("payment-pending", function (req, res) {
+    res.render("suceess", req.query)
+})
+
+app.get("payment-failure", function (req, res) {
+    res.render("suceess", req.query)
+})
 
 app.listen(port);
---
+++ payments.js
@@ -0,0 +1,5 @@
+var Payments = {
+    
+}
+
+export default Payments;
